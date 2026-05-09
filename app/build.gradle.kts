41cbfa9feb2d54079d93a5618d22d6ef6bf24262
adiamas <robert.hak@gmail.com>
2026-05-08 23:46:11 -0400

    Upgrade AGP to 9.2.1 and Kotlin to 2.2.10
    - Remove deprecated gradle.properties flags
    - Switch to AGP built-in Kotlin plugin management
    - Replace kotlinOptions/compileOptions with kotlin { jvmToolchain(17) }
    - Add kotlin.compose plugin for Kotlin 2.x Compose support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -22,22 +22,13 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
     }
+}
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.13"
-    }
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
